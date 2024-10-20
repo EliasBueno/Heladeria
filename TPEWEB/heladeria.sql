d5c7e6e03db673e35e85265166bcc39d0881a6af
EliasBueno <142538980+EliasBueno@users.noreply.github.com>
2024-10-20 09:55:25 -0300

    Update heladeria.sql

--- TPEWEB/heladeria.sql
+++ TPEWEB/heladeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-09-2024 a las 22:38:17
+-- Tiempo de generación: 20-10-2024 a las 14:53:40
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.1.25
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `heladeria`
+-- Base de datos: `heladeriaa`
 --
 
 -- --------------------------------------------------------
@@ -31,8 +31,25 @@ CREATE TABLE `productos` (
   `idProducto` int(11) NOT NULL,
   `tipo` varchar(45) NOT NULL,
   `valor` int(11) NOT NULL,
-  `sabor` varchar(45) NOT NULL
+  `sabor` varchar(45) NOT NULL,
+  `id_sucursal` int(11) NOT NULL,
+  `imagen` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`idProducto`, `tipo`, `valor`, `sabor`, `id_sucursal`, `imagen`) VALUES
+(1, 'Helado', 2000, 'Super Gridito', 1, 'SuperGridito2.jpg'),
+(3, 'paleta', 2250, 'Frutilla ', 1, 'paleta_frutilla.jpg'),
+(4, 'Postre', 7000, 'DDL Brownie', 3, 'PostreFiglio.jpg'),
+(13, 'Helado ', 4000, 'DDL iglu', 2, 'HeladoDDL.jpg'),
+(16, 'Palito', 5000, 'Bombon chocolate', 1, 'PalitoBombonGrido.jpg'),
+(19, 'Postre', 6000, 'DDL y Vainilla', 2, 'PostreIglu.jpg'),
+(20, 'Promo', 700, 'Chocolate, Vainilla y Americana', 2, 'PromoIglu.jpg'),
+(22, 'Macarrones', 10000, 'Cafe,Chocolate, Pistacho,Frutilla', 3, 'MacarronesFiglio.jpg'),
+(23, 'Helado', 3000, 'DDL Figlio', 3, 'HeladoDDLFiglio.jpg');
 
 -- --------------------------------------------------------
 
@@ -44,8 +61,37 @@ CREATE TABLE `sucursales` (
   `idSucursal` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `ubicacion` varchar(45) NOT NULL,
-  `productos` int(11) NOT NULL
+  `imagen` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `sucursales`
+--
+
+INSERT INTO `sucursales` (`idSucursal`, `nombre`, `ubicacion`, `imagen`) VALUES
+(1, 'Grido', 'Tandiil', 'grido.jpg'),
+(2, 'Iglu', 'Mar del Plata', 'iglu.jpg'),
+(3, 'Figlio', 'Tandil', 'Figlio.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('admin','user') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `usuario`, `password`, `rol`) VALUES
+(0, 'webadmin', '$2y$10$Iiwwn2goWeqONvmA.4lFhO3dZ4CE/n6kYEwNLHSEx0Ax3Cv/lJdIm', 'user');
 
 --
 -- Índices para tablas volcadas
@@ -55,14 +101,21 @@ CREATE TABLE `sucursales` (
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`idProducto`);
+  ADD PRIMARY KEY (`idProducto`),
+  ADD KEY `id_sucursal` (`id_sucursal`);
 
 --
 -- Indices de la tabla `sucursales`
 --
 ALTER TABLE `sucursales`
-  ADD PRIMARY KEY (`idSucursal`),
-  ADD UNIQUE KEY `fk_productos` (`productos`);
+  ADD PRIMARY KEY (`idSucursal`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -72,23 +125,29 @@ ALTER TABLE `sucursales`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `sucursales`
 --
 ALTER TABLE `sucursales`
-  MODIFY `idSucursal` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idSucursal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `sucursales`
+-- Filtros para la tabla `productos`
 --
-ALTER TABLE `sucursales`
-  ADD CONSTRAINT `sucursales_ibfk_1` FOREIGN KEY (`productos`) REFERENCES `productos` (`idProducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`idSucursal`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
